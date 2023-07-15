a5a75bdb7438329ab2c4aae46a6ce57a450cff9d
Anton Cheng <antonassocareer@gmail.com>
2023-07-15 13:49:41 +0800

    chore: fix deployment

--- site.config.js
+++ site.config.js
@@ -27,7 +27,7 @@ const CONFIG = {
   link: "https://morethan-log.vercel.app",
   since: "2023", // If leave this empty, current year will be used.
   lang: "zh-TW", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
-  // ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
+  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
   seo: {
     keywords: ["solidity", "defi", "life", "gas optimization"],
   },
