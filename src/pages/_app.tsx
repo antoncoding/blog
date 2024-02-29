85c752d8cb061f99832f5be286461c85cd429703
Anton Cheng <antonassocareer@gmail.com>
2024-02-29 14:56:42 +0800

    chore: override some notion stylings

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,19 @@ import { Hydrate, QueryClientProvider } from "@tanstack/react-query"
 import { RootLayout } from "src/layouts"
 import { queryClient } from "src/libs/react-query"
 
+
+// core styles shared by all of react-notion-x (required)
+import "react-notion-x/src/styles.css"
+
+// used for code syntax highlighting (optional)
+import "prismjs/themes/prism-tomorrow.css"
+
+// used for rendering equations (optional)
+import "katex/dist/katex.min.css"
+
+// custom overrides
+import "src/styles/notion.css"
+
 function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => page)
 
--- src/routes/Detail/components/NotionRenderer/index.tsx
+++ src/routes/Detail/components/NotionRenderer/index.tsx
@@ -4,15 +4,6 @@ import Link from "next/link"
 import { ExtendedRecordMap } from "notion-types"
 import useScheme from "src/hooks/useScheme"
 
-// core styles shared by all of react-notion-x (required)
-import "react-notion-x/src/styles.css"
-
-// used for code syntax highlighting (optional)
-import "prismjs/themes/prism-tomorrow.css"
-
-// used for rendering equations (optional)
-
-import "katex/dist/katex.min.css"
 import { FC } from "react"
 import styled from "@emotion/styled"
 
