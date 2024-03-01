aaa2421fcbd3c160325ab663597af7b9161af629
Anton Cheng <antonassocareer@gmail.com>
2024-03-01 11:50:09 +0800

    chore: remove about, increase line height

--- src/layouts/RootLayout/Header/NavBar.tsx
+++ src/layouts/RootLayout/Header/NavBar.tsx
@@ -2,7 +2,8 @@ import styled from "@emotion/styled"
 import Link from "next/link"
 
 const NavBar: React.FC = () => {
-  const links = [{ id: 1, name: "About", to: "/about" }]
+  const links: {id: number, to: string, name: string}[]  = []
+
   return (
     <StyledWrapper className="">
       <ul>
